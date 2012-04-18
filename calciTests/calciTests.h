de3efc38b97ac37c1e3c73affb6a6e54002caea4
Saikat Maitra <saikat.maitra@gmail.com>
2012-04-18 23:58:14 +0530

    Initial Commit

---
+++ calciTests/calciTests.h
@@ -0,0 +1,13 @@
+//
+//  calciTests.h
+//  calciTests
+//
+//  Created by Saikat Maitra on 18/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface calciTests : SenTestCase
+
+@end
---
+++ calciTests/calciTests.m
@@ -0,0 +1,32 @@
+//
+//  calciTests.m
+//  calciTests
+//
+//  Created by Saikat Maitra on 18/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "calciTests.h"
+
+@implementation calciTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in calciTests");
+}
+
+@end
